c4a315e26566b6790343af3002cb26af554f158a
TETHERED <xtethered@gmail.com>
2025-12-08 19:46:46 -0600

    Added some JavaScript

--- comics/comics.js
+++ comics/comics.js
@@ -1,5 +1,59 @@
 pageCount = 0;
 
-function turnPage(){
-    console.log("wassup?");
+function backPage()
+{
+    //alert("WASSUP?");
+    if (pageCount > 0){
+        pageCount = pageCount - 1;
+    }
+    switch (pageCount){
+        case 0:
+            document.getElementById("comic-page").src = "../images/comics/pu1/PU_Chap1_1.png";
+            break;
+        case 1:
+            document.getElementById("comic-page").src = "../images/comics/pu1/PU_Chap1_2.png";
+            break;
+        case 2:
+            document.getElementById("comic-page").src = "../images/comics/pu1/PU_Chap1_3.png";
+            break;
+        case 3:
+            document.getElementById("comic-page").src = "../images/comics/pu1/PU_Chap1_4.png";
+            break;
+        default:
+            document.getElementById("comic-page").src = "../images/comics/pu1/PU_Chap1_1.png";
+            break;
+    }
+    //alert(pageCount);
+}
+
+function nextPage()
+{
+    if (pageCount < 3) {
+        pageCount = pageCount + 1;
+    }
+    switch (pageCount){
+        case 0:
+            document.getElementById("comic-page").src = "../images/comics/pu1/PU_Chap1_1.png";
+            break;
+        case 1:
+            document.getElementById("comic-page").src = "../images/comics/pu1/PU_Chap1_2.png";
+            break;
+        case 2:
+            document.getElementById("comic-page").src = "../images/comics/pu1/PU_Chap1_3.png";
+            break;
+        case 3:
+            document.getElementById("comic-page").src = "../images/comics/pu1/PU_Chap1_4.png";
+            break;
+        default:
+            document.getElementById("comic-page").src = "../images/comics/pu1/PU_Chap1_1.png";
+            break;
+    }
+    //alert(pageCount);
+}
+
+function startOver()
+{
+    pageCount = 0;
+    document.getElementById("comic-page").src = "../images/comics/pu1/PU_Chap1_1.png";
+    //alert(pageCount);
 }
